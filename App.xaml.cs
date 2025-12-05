da3cac67de954a6ac05ae42dc4cdbef458fa1cc4
Roy4Github <49321053+roymjohn@users.noreply.github.com>
2025-12-05 07:15:35 -0500

    initial

---
+++ App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using FastNLose.Pages;
+using FastNLose.Services;
+using System.Globalization;
+
+namespace FastNLose;
+
+
+public partial class App : Application
+{
+    public static DatabaseService Database { get; private set; }
+    public static string dbPath { get; private set; }
+
+    public App()
+    {
+        InitializeComponent();
+        dbPath = Path.Combine(FileSystem.AppDataDirectory, "fastnlose.db3");
+        Database = new DatabaseService(dbPath);
+        MainPage = new NavigationPage(new Pages.MainPage());
+    }
+
+    protected override void OnResume()
+    {
+        MainThread.BeginInvokeOnMainThread(() =>
+        {
+            if (Current?.MainPage is MainPage mp)
+                mp.ForceDailyRefresh();
+        });
+    }
+
+}
+
+public class InverseBoolConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        => !(bool)value;
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        => !(bool)value;
+}
---
+++ MauiProgram.cs
@@ -0,0 +1,37 @@
+﻿
+using FastNLose.Services;
+using Microsoft.Extensions.Logging;
+using Plugin.LocalNotification;
+
+namespace FastNLose
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+
+            var builder = MauiApp.CreateBuilder();         
+       
+
+            builder
+                .UseMauiApp<App>()
+                .UseLocalNotification()                
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+            string dbPath = Path.Combine(FileSystem.AppDataDirectory, "FastNLose.db3");
+            builder.Services.AddSingleton(new DatabaseService(dbPath));
+
+            builder.Services.AddTransient<Pages.MainPage>();
+            builder.Services.AddTransient<Pages.SettingsPage>();
+
+#if DEBUG
+            builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
